ebbba2bc378c3660c85b3e1d4618d40823cec9cd
Swapnil Sharma <swap.sha96@gmail.com>
2017-09-18 13:36:20 +0530

    Error handling in all functions

--- omnicorn/__init__.py
+++ omnicorn/__init__.py
@@ -5,51 +5,66 @@ API_URL = "http://narwhal.astro.yale.edu:8080"
 
 def singleobject(id):
 	params = {}
-	if params['id'] is not None:
+	if id is not None:
 		params['id'] = id
 	r = requests.get(API_URL + "/singleobject", params = params)
-	result = r.json()
+	if r.status_code == 200:
+		result = r.json()
+	else:
+		result = r.content
 	return result
 
 def conesearch(ra, dec, radius, unit = 'd'):
 	params = {}
-	if params['ra'] is not None:
+	if ra is not None:
 		params['ra'] = ra
-	if params['dec'] is not None:
+	if dec is not None:
 		params['dec'] = dec
-	if params['radius'] is not None:
+	if radius is not None:
 		params['radius'] = radius
 	params['unit'] = unit
 	r = requests.get(API_URL + "/conesearch", params = params)
-	result = r.json()
+	if r.status_code == 200:
+		result = r.json()
+	else:
+		result = r.content
 	return result
 
 def nearest(ra, dec, unit = 'd'):
 	params = {}
-	if params['ra'] is not None:
+	if ra is not None:
 		params['ra'] = ra
-	if params['dec'] is not None:
+	if dec is not None:
 		params['dec'] = dec
-	params['unit'] = unit
+	unit = unit
 	r = requests.get(API_URL + "/nearest", params = params)
-	result = r.json()
+	if r.status_code == 200:
+		result = r.json()
+	else:
+		result = r.content
 	return result
 
 def pointing(ra, dec, unit = 'd'):
 	params = {}
-	if params['ra'] is not None:
+	if ra is not None:
 		params['ra'] = ra
-	if params['dec'] is not None:
+	if dec is not None:
 		params['dec'] = dec
 	params['unit'] = unit
 	r = requests.get(API_URL + "/pointing", params = params)
-	result = r.json()
+	if r.status_code == 200:
+		result = r.json()
+	else:
+		result = r.content
 	return result
 
 def pointing_id(id):
 	params = {}
-	if params['id'] is not None:
+	if id is not None:
 		params['id'] = id
 	r = requests.get(API_URL + "/pointing_id", params = params)
-	result = r.json()
+	if r.status_code == 200:
+		result = r.json()
+	else:
+		result = r.content
 	return result
